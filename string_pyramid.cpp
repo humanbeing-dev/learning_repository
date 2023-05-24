89df119dfd6e1521c696489d7468379fa7048348
Maciej Zamojski <133371097+Zam-Ma@users.noreply.github.com>
2023-05-24 22:28:04 +0200

    Updated string_pyramid.cpp with shorter and cleaner code.
    Removed unnecessary clear function for leading string by reinitiating the leading string at each iteration of the 'fo'r loop.

--- string_pyramid.cpp
+++ string_pyramid.cpp
@@ -4,41 +4,6 @@
 using namespace std;
 
 int main(){
-    
-/***********************
-Old code before revision. 
-************************/
-
-//    cout << "Enter the text for the pyramid: ";
-//    string message {};
-//    string leading {};
-//    getline(cin, message);
-//    cout << endl;
-//    for (size_t i{}; i < message.size(); ++i){
-//        if (i == message.size() - 1){
-//            cout << message.substr(0,message.size());
-//            for (int j = message.size() - 2; j >= 0; j--)
-//                cout << message.at(j);
-//        }
-//        else {
-//            for (size_t j{}; j < message.size() - 1 - i; ++j){
-//                leading += ' ';
-//            }
-//            cout << leading << message.substr(0,i+1);
-//            for (int j = i-1; j >= 0; j--){
-//                cout << message.at(j);
-//                
-//            }
-//            cout << endl;
-//            leading.clear();  //!!!!! If we initialize leading at each iteration we wouldn't have to clear the leading string.!!!!!
-//        }
-//    }
-//    cout << endl;
-    
-
-/***********************
-New code after revision. 
-************************/
     cout << "Enter the text for the pyramid: ";
     string message {};
     getline(cin, message);
